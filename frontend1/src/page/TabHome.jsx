5d13f8ba112c0ba741047d2e0dfc834464d00bf1
chunwei <tanchunwei@gmail.com>
2020-09-11 15:41:35 +0800

    Add elastic ui example in microfrontend 1

--- frontend1/src/page/TabHome.jsx
+++ frontend1/src/page/TabHome.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { EuiFlexGroup, EuiFlexItem, EuiAvatar, EuiComment, EuiText } from '@elastic/eui'
+import '@elastic/eui/dist/eui_theme_light.css';
 
 const TabHome = () => {
     const [messages, setMessages] = useState([])
     const handleEvent = (msg) => {
-        setMessages([...messages, msg])
+        var name = "Frontend 2"
+        var dt = new Date().toLocaleString();
+        setMessages([{
+            name,
+            message: msg,
+            dt: dt
+        }, ...messages])
     }
 
     useEffect(() => {
@@ -14,10 +22,38 @@ const TabHome = () => {
 
     return (
         <div>
-            This is frontend1 tab home
-            {messages.map((msg) => {
-                return (<div>{msg}</div>)
-            })}
+            <EuiFlexGroup>
+                <EuiFlexItem grow={1}/>
+                <EuiFlexItem grow={2}>
+                    <EuiAvatar size="m" name="Frontend 1" />
+                </EuiFlexItem>
+                <EuiFlexItem grow={7}/>
+            </EuiFlexGroup>
+
+            <EuiFlexGroup>
+                <EuiFlexItem grow={1}/>
+                <EuiFlexItem grow={3}>
+                    {messages.map((m) => {
+                        return (
+                            <EuiComment
+                                username={m.name}
+                                event="send a message"
+                                timestamp={m.dt}
+                                timelineIcon={
+                                    <EuiAvatar size="s" name={m.name} />
+                                }
+                                >
+                                <EuiText size="s" textAlign="left">
+                                    <p>
+                                    {m.message}
+                                    </p>
+                                </EuiText>
+                            </EuiComment>
+                        )
+                    })}
+              </EuiFlexItem>
+              <EuiFlexItem grow={6}/>
+            </EuiFlexGroup>
         </div>
     );
 }
